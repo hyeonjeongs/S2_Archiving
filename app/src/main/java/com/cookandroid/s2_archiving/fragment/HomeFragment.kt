06399803fc6cb960d025e337589c56ba34dcb0a1
EUNA-319 <81363864+EUNA-319@users.noreply.github.com>
2022-01-28 00:54:22 +0900

    [modify] Fragment에 맞춰 데이터베이스 연결

--- app/src/main/java/com/cookandroid/s2_archiving/fragment/HomeFragment.kt
+++ app/src/main/java/com/cookandroid/s2_archiving/fragment/HomeFragment.kt
@@ -2,18 +2,52 @@ package com.cookandroid.s2_archiving.fragment
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cookandroid.s2_archiving.*
+import com.cookandroid.s2_archiving.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    //위젯 연결할 변수 선언
+    lateinit var adapter : RecyclerView.Adapter<FriendDataAdapter.CustomViewHolder>
+    lateinit var layoutManager: RecyclerView.LayoutManager
+    lateinit var friendDataList: ArrayList<FriendData>
+
+
+    //프레그먼트를 위한 변수들
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var likeFragment: LikeFragment
+    private lateinit var userFragment: UserFragment
+
+    //파이어베이스
+    private lateinit var database : FirebaseDatabase
+    private lateinit var mDatabaseRef : DatabaseReference
+    private var mFirebaseAuth: FirebaseAuth? = null //파이어베이스 인증
+
+
+    //
+    var photoUri: Uri? = null
+
+
+    lateinit var cameraPermission: ActivityResultLauncher<String>
+    lateinit var storagePermission : ActivityResultLauncher<String>
+
+    lateinit var cameraLauncher: ActivityResultLauncher<Uri>
+    lateinit var galleryLauncher: ActivityResultLauncher<String>
+
     //정적으로 사용되는 부분
     companion object {
         fun newInstance() : HomeFragment{
@@ -33,6 +67,8 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+
+
         //친구 추가 버튼에 클릭 리스너 연결
         ivPlus.setOnClickListener {
             activity?.let{
@@ -50,29 +86,49 @@ class HomeFragment : Fragment() {
 
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { // <-- 리사이클러뷰 넣어보기
         super.onViewCreated(view, savedInstanceState)
 
-        //
-        // recyclerview 데이터
-        val profileList = arrayListOf(
-                Profiles(R.drawable.woman, "조윤진", R.drawable.star_empty),
-                Profiles(R.drawable.man, "김씨", R.drawable.star_full),
-                Profiles(R.drawable.woman, "이땡땡", R.drawable.star_full),
-                Profiles(R.drawable.woman, "최씨", R.drawable.star_empty),
-                Profiles(R.drawable.man, "박땡땡", R.drawable.star_full),
-                Profiles(R.drawable.woman, "신땡땡", R.drawable.star_empty),
-                Profiles(R.drawable.man, "윤씨", R.drawable.star_empty),
-                Profiles(R.drawable.woman, "권씨", R.drawable.star_empty),
-                Profiles(R.drawable.woman, "강씨", R.drawable.star_empty),
-                Profiles(R.drawable.man, "서씨", R.drawable.star_full)
 
-        )
 
-        rvProfile.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
-        //layoutManager = LinearLayoutManager(this.context)
-        rvProfile.setHasFixedSize(true)//리사이클러뷰 성능 강화
-        rvProfile.adapter = ProfileAdapter(profileList)
+        //파이어베이스 계정, 리얼타임 데이터베이스
+        mFirebaseAuth = FirebaseAuth.getInstance()
+
+
+
+        friendDataList = ArrayList<FriendData>() //FriendData 객체를 담을 ArrayList
+
+        database = FirebaseDatabase.getInstance() //파이어베이스 데이터베이스 연동
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Firebase")
+
+        //리사이클러뷰에 담을 데이터 가져오기(selectedItem 태그를 통해서 보여줄 게시글 구분)
+        mDatabaseRef.child("UserFriends").child("${mFirebaseAuth!!.currentUser!!.uid}")
+            .orderByChild("timestamp").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    friendDataList.clear()
+
+                    for (data : DataSnapshot in snapshot.getChildren()) {
+                        var friendData : FriendData? = data.getValue(FriendData::class.java)
+
+                        friendDataList.add(friendData!!) //담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+
+                        Log.d("태그", "$friendDataList")
+                    }
+                    adapter.notifyDataSetChanged() //리스트 저장 및 새로고침
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+            })
+
+        adapter = FriendDataAdapter(friendDataList, this.requireContext())
+        rvProfile.setAdapter(adapter)
+
+//        rvProfile.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+//        //layoutManager = LinearLayoutManager(this.context)
+//        rvProfile.setHasFixedSize(true)//리사이클러뷰 성능 강화
+//        rvProfile.adapter = FriendDataAdapter(friendDataList)
 
 
     }
