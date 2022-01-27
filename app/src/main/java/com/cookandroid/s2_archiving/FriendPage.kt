804ec2cf05f9bbcb4c6a61077de713773feaffcb
hyeonjeong Park <py0429@ewhain.net>
2022-01-27 21:57:56 +0900

    frament revise

--- app/src/main/java/com/cookandroid/s2_archiving/FriendPage.kt
+++ app/src/main/java/com/cookandroid/s2_archiving/FriendPage.kt
@@ -12,20 +12,18 @@ class FriendPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friend_page)
         val friendlist = arrayListOf( //리스트형태의 객체들을 넣어줌
-                FriendModel(R.drawable.ic_account,20220202,"코테이토",R.drawable.ic_baseline_favorite_24),
-                FriendModel(R.drawable.ic_account,20220203,"사랑해요",R.drawable.ic_baseline_favorite_24),
+                FriendModel(R.drawable.ic_account,20220202,"아카이빙",R.drawable.ic_baseline_favorite_24),
+                FriendModel(R.drawable.ic_account,20220203,"화이팅",R.drawable.ic_baseline_favorite_24),
                 FriendModel(R.drawable.ic_account,20221010,"♥",R.drawable.ic_baseline_favorite_border_24),
-                FriendModel(R.drawable.ic_account,20221013,"모두",R.drawable.ic_baseline_favorite_24),
-                FriendModel(R.drawable.ic_account,20221015,"메리설날",R.drawable.ic_baseline_favorite_24),
-                FriendModel(R.drawable.ic_account,20221015,"다들",R.drawable.ic_baseline_favorite_border_24),
-                FriendModel(R.drawable.ic_account,20220916,"대면참여하고",R.drawable.ic_baseline_favorite_24),
-                FriendModel(R.drawable.ic_account,20221010,"번개열어요ㅠㅠ",R.drawable.ic_baseline_favorite_border_24)
+                FriendModel(R.drawable.ic_account,20221013,"안드로이드",R.drawable.ic_baseline_favorite_24),
+                FriendModel(R.drawable.ic_account,20221015,"열심히",R.drawable.ic_baseline_favorite_24),
+                FriendModel(R.drawable.ic_account,20221015,"하기",R.drawable.ic_baseline_favorite_border_24)
         )
-        val rvprofile = findViewById<RecyclerView>(R.id.rv_profile)
-        rvprofile.layoutManager = GridLayoutManager(this,2) //한 행에 두 열씩
-        rvprofile.setHasFixedSize(true) //리사이클러뷰 성능 개선
+        val rvfriend = findViewById<RecyclerView>(R.id.rv_friend)
+        rvfriend.layoutManager = GridLayoutManager(this,2) //한 행에 두 열씩
+        rvfriend.setHasFixedSize(true) //리사이클러뷰 성능 개선
 
-        rvprofile.adapter = FriendAdapter(friendlist) //위에서 선언한 리스트를 어뎁터에 변수인 profileList로 전달
+        rvfriend.adapter = FriendAdapter(friendlist) //위에서 선언한 리스트를 어뎁터에 변수인 profileList로 전달
 
     }
 }
--- app/src/main/java/com/cookandroid/s2_archiving/fragment/HomeFragment.kt
+++ app/src/main/java/com/cookandroid/s2_archiving/fragment/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.cookandroid.s2_archiving.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import com.cookandroid.s2_archiving.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cookandroid.s2_archiving.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
     //정적으로 사용되는 부분
@@ -16,16 +21,6 @@ class HomeFragment : Fragment() {
         }
     }
 
-    //메모리에 올라갔을때
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    //프레그먼트를 안고 있는 엑티비티에 붙었을 때
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
     //뷰가 생성되었을때
     //프레그먼트와 레이아웃 연결시켜줌
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -33,4 +28,67 @@ class HomeFragment : Fragment() {
 
         return view
     }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //친구 추가 버튼에 클릭 리스너 연결
+        ivPlus.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, FriendAdd::class.java)
+                startActivity(intent)
+            }
+        }
+
+        btnSearch.setOnClickListener {
+            activity?.let{
+                val intent = Intent(context, ModifyAccount::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //
+        // recyclerview 데이터
+        val profileList = arrayListOf(
+                Profiles(R.drawable.woman, "조윤진", R.drawable.star_empty),
+                Profiles(R.drawable.man, "김씨", R.drawable.star_full),
+                Profiles(R.drawable.woman, "이땡땡", R.drawable.star_full),
+                Profiles(R.drawable.woman, "최씨", R.drawable.star_empty),
+                Profiles(R.drawable.man, "박땡땡", R.drawable.star_full),
+                Profiles(R.drawable.woman, "신땡땡", R.drawable.star_empty),
+                Profiles(R.drawable.man, "윤씨", R.drawable.star_empty),
+                Profiles(R.drawable.woman, "권씨", R.drawable.star_empty),
+                Profiles(R.drawable.woman, "강씨", R.drawable.star_empty),
+                Profiles(R.drawable.man, "서씨", R.drawable.star_full)
+
+        )
+
+        rvProfile.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+        //layoutManager = LinearLayoutManager(this.context)
+        rvProfile.setHasFixedSize(true)//리사이클러뷰 성능 강화
+        rvProfile.adapter = ProfileAdapter(profileList)
+
+
+    }
+
+
+    //메모리에 올라갔을때
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+    }
+
+    //프레그먼트를 안고 있는 엑티비티에 붙었을 때
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
 }
