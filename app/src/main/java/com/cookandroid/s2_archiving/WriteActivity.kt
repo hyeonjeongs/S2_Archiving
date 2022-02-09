a9801ffd6a86c720a5fe7fbf3973db38e24e4073
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-02-09 21:53:13 +0900

    [add] 게시글 Firebase에 저장하는 기능 추가

--- app/src/main/java/com/cookandroid/s2_archiving/WriteActivity.kt
+++ app/src/main/java/com/cookandroid/s2_archiving/WriteActivity.kt
@@ -1,12 +1,18 @@
 package com.cookandroid.s2_archiving
 
 import android.Manifest
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_write.*
 
@@ -23,11 +29,27 @@ class WriteActivity : AppCompatActivity() {
 
     var imgUrl : String = ""
 
+    private lateinit var mFirebaseAuth: FirebaseAuth // 파이어베이스 인증 처리
+    private lateinit var mDatabaseRef: DatabaseReference // 실시간 데이터 베이스
+
+    private lateinit var mEtDate: EditText // 날짜
+    private lateinit var mEtDateName: EditText // 기념일 이름
+    private lateinit var mEtPost: EditText // 글 내용
+    private lateinit var mBtnPostRegister: Button // 게시글 업로드 버튼
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
 
+    //파이어베이스 계정, 리얼타임 데이터베이스
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Firebase")
 
+        mEtDate = findViewById<EditText>(R.id.etWriteDate)
+        mEtDateName = findViewById<EditText>(R.id.etWriteName)
+        mEtPost = findViewById<EditText>(R.id.etWritePost)
+        mBtnPostRegister = findViewById<Button>(R.id.btnWriteRegister)
 
         storagePermission=registerForActivityResult(
             ActivityResultContracts.RequestPermission()
@@ -61,6 +83,33 @@ class WriteActivity : AppCompatActivity() {
         }
 
         storagePermission.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        mBtnPostRegister.setOnClickListener{
+            val hashMap : HashMap<String, String> = HashMap()
+
+            var strDate: String = mEtDate.text.toString()
+            var strDateName: String = mEtDateName.text.toString()
+            var strPost: String = mEtPost.text.toString()
+
+
+            hashMap.put("uid", mFirebaseAuth!!.currentUser!!.uid)
+            hashMap.put("postDate", strDate)
+            hashMap.put("postDateName", strDateName)
+            hashMap.put("post", strPost)
+
+            mDatabaseRef.ref.child("UserPosts").child("${mFirebaseAuth!!.currentUser!!.uid}").push().setValue(hashMap)
+                    .addOnCompleteListener {
+                        if(it.isSuccessful){
+                            Toast.makeText(this, "등록완료", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+            Toast.makeText(this, "게시글 추가 완료", Toast.LENGTH_SHORT).show()
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
 
     }
 
