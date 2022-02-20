16f1bedaadf66b34202619b74e0a74fd9f7f25ef
hyeonjeong Park <py0429@ewhain.net>
2022-02-20 12:41:04 +0900

    친구게시글 heart 반영되도록 수정

--- app/src/main/java/com/cookandroid/s2_archiving/fragment/LikeFragment.kt
+++ app/src/main/java/com/cookandroid/s2_archiving/fragment/LikeFragment.kt
@@ -1,5 +1,6 @@
 package com.cookandroid.s2_archiving.fragment
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,13 +10,27 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.cookandroid.s2_archiving.FavoriteAdapter
+import com.cookandroid.s2_archiving.FriendDataAdapter
 import com.cookandroid.s2_archiving.R
 import com.cookandroid.s2_archiving.model.MyFavorite
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_like.*
 import kotlinx.android.synthetic.main.fragment_like.view.*
 
 class LikeFragment : Fragment() {
     var fragmentView : View? = null
+
+    //파이어베이스
+    private lateinit var database : FirebaseDatabase
+    private lateinit var mDatabaseRef : DatabaseReference //실시간 데이터베이스 연결
+    private var mFirebaseAuth: FirebaseAuth? = null
+    var uid : String? = null
+    var auth : FirebaseAuth? = null
+
+    // context
+    private lateinit var activity: Activity
 
     companion object{
         fun newInstance() : LikeFragment{
@@ -31,13 +46,16 @@ class LikeFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? { //like_fragment xml파일이랑 연결
-        val friendlist = arrayListOf( //리스트형태의 객체들을 넣어줌
-            MyFavorite(R.drawable.food3,"박씨",20220201,"#재밌는날",R.drawable.ic_baseline_favorite_24),
-            MyFavorite(R.drawable.food2,"김씨",20220501,"#깜짝놀란날",R.drawable.ic_baseline_favorite_24)
-        )
-
         val fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_like,container, false)
-        fragmentView?.fragmentlike_rv?.adapter = FavoriteAdapter(friendlist)
+        //파이어베이스 계정, 리얼타임 데이터베이스
+        mFirebaseAuth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance() //파이어베이스 데이터베이스 연동
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("Firebase")
+        auth = FirebaseAuth.getInstance()
+        uid = arguments?.getString("favoriteUid")
+
+
+        fragmentView?.fragmentlike_rv?.adapter = FavoriteAdapter(activity,this)
         fragmentView?.fragmentlike_rv?.layoutManager = GridLayoutManager(activity,2)
 
         return fragmentView
