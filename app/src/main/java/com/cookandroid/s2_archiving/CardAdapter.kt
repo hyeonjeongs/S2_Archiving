3b6db87b7f3105196a05f9ca49b5a71bfc4af6c6
EUNA-319 <81363864+EUNA-319@users.noreply.github.com>
2022-02-23 03:02:58 +0900

    [cardView] 데이터 연결 및 이동

--- app/src/main/java/com/cookandroid/s2_archiving/CardAdapter.kt
+++ app/src/main/java/com/cookandroid/s2_archiving/CardAdapter.kt
@@ -1,6 +1,7 @@
 package com.cookandroid.s2_archiving
 
 import android.content.Context
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,16 +11,18 @@ import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.cookandroid.s2_archiving.fragment.ViewoneFragment
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 import com.google.firebase.storage.FirebaseStorage
 
 class CardAdapter(val postDataList : ArrayList<PostData>, val context: Context, val fragment_s: Fragment) : RecyclerView.Adapter<CardAdapter.CustomViewHolder>() {
     private var mFirebaseAuth: FirebaseAuth? = FirebaseAuth.getInstance() //파이어베이스 인증
-    private var mDatabaseRef: DatabaseReference =
-        FirebaseDatabase.getInstance().getReference("Firebase")//실시간 데이터베이스
+    private var mDatabaseRef: DatabaseReference = FirebaseDatabase.getInstance().getReference("Firebase")//실시간 데이터베이스
     private lateinit var fbStorage: FirebaseStorage
+
+    private lateinit var fId:String
+    private var fName:String = ""
 
     private var activity: MainActivity? = null//메인에 함수 부르기 위해 선언하기
 
@@ -40,10 +43,37 @@ class CardAdapter(val postDataList : ArrayList<PostData>, val context: Context, 
         }
         holder.cvdate.text = postDataList[position].postDate
         holder.cvspecial.text = postDataList[position].postDateName
-        holder.cvfriendname.text = postDataList[position].postFriendId //수정하기
-        /*Glide.with(holder.itemView).load(postDataList[position].postPhotoUri).into(holder.postimage)
-        holder.date.text = postDataList[position].postDate
-        holder.special.text = postDataList[position].postDateName*/
+        fId = postDataList[position].postFriendId
+        // 친구 이름 가져오기
+        mDatabaseRef.child("UserFriends").child("${mFirebaseAuth?.currentUser!!.uid}").child(fId).addValueEventListener(object :
+            ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.value == null) { // 널이면 아무것도하지마
+
+                }
+                else { // 카드뷰 수정되면 프로필 이미지 로드
+                    var friend:FriendData? = snapshot.getValue(FriendData::class.java)
+                    fName = friend!!.fName
+                    holder.cvfriendname.text = fName
+                }
+            }
+        })
+
+
+        holder.cvimage.setOnClickListener { // 카드뷰로 나중에 바꾸기
+            var fragment: Fragment = ViewoneFragment()
+            var bundle: Bundle = Bundle()
+            fragment.arguments = bundle
+            bundle.putString("friend_id", postDataList[position].postFriendId)
+            bundle.putString("post_id",postDataList[position].postId)
+            bundle.putString("id","cardview_adapter")
+            activity = fragment_s.activity as MainActivity?
+            activity?.fragemtChage_for_adapter_view(fragment)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -55,10 +85,6 @@ class CardAdapter(val postDataList : ArrayList<PostData>, val context: Context, 
         val cvdate = itemView.findViewById<TextView>(R.id.dayNumber)
         val cvfriendname = itemView.findViewById<TextView>(R.id.fName)
         val cvspecial = itemView.findViewById<TextView>(R.id.dayName)
-        /*
-        val postimage = itemView.findViewById<ImageView>(R.id.post_img)
-        val date = itemView.findViewById<TextView>(R.id.post_date)
-        val special = itemView.findViewById<TextView>(R.id.post_special)
-        val heart = itemView.findViewById<ImageView>(R.id.post_heart)*/
+
     }
 }
